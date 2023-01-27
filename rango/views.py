416554120c148fdc7d163df040f714df18bc8d09
Nathan Logie <2658532l@student.gla.ac.uk>
2023-01-27 14:43:54 +0000

    Fixed test weirdness

--- rango/views.py
+++ rango/views.py
@@ -9,4 +9,5 @@ def index(request):
 
 def about(request):
     response = "Rango says here is the about page." + '<a href="/rango/">Index</a>'
-    return render(request, 'rango/about.html')
+    context_dict = {'boldmessage': 'Crunchy, creamy, cookie, candy, cupcake!'}
+    return render(request, 'rango/about.html', context=context_dict)
--- tango_with_django_project/urls.py
+++ tango_with_django_project/urls.py
@@ -25,4 +25,3 @@ urlpatterns = [
     path('admin/', admin.site.urls),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
-co = ""
